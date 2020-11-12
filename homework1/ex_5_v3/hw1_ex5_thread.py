e344fae0df1719a54213ee4602e0239275bdba02
Irene Benedetto <irenebenedetto@Irenes-MacBook-Pro.local>
2020-11-12 13:08:51 +0100

    changed chunk size

--- homework1/ex_5_v3/hw1_ex5_thread.py
+++ homework1/ex_5_v3/hw1_ex5_thread.py
@@ -4,13 +4,16 @@ import wave
 import time
 import sys
 import numpy as np
-from os import path
+import os
+import sys
 from scipy.io import wavfile
 from scipy import signal
 import tensorflow as tf
 from subprocess import Popen
 import argparse
 from threading import Thread
+
+os.close(sys.stderr.fileno()) 
 
 def performance():
     Popen(['sudo sh -c "echo performance > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor"'], shell=True)
@@ -26,40 +29,48 @@ parameter = parser.parse_args()
 num_sample = parameter.num_samples
 output_file = parameter.output
 
-RATE, CHANNELS, CHUNK_SIZE, L, FORMAT = 48000, 1, 1024, 1, pyaudio.paInt16
+RATE, CHANNELS, CHUNK_SIZE, L, FORMAT = 48000, 1, 2400, 1, pyaudio.paInt16
 output_fts, num_mel_bins, lower_frequency, upper_frequency = 10, 40, 20, 4000
 UP, DOWN = 1, 3
 frame_length, frame_step = 640, 320
 
+n_cycles = int(RATE/CHUNK_SIZE)*L
+
+print(f'Chunks {n_cycles}')
 buffer = io.BytesIO()
 num_spectrogram_bins = 321
 linear_to_mel_weight_matrix = tf.signal.linear_to_mel_weight_matrix(num_mel_bins, num_spectrogram_bins, RATE, lower_frequency, upper_frequency)
 linear_to_mel_weight_matrix = tf.cast(linear_to_mel_weight_matrix, dtype=tf.float32)
 
 p = pyaudio.PyAudio()
-stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True)
+stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK_SIZE)
+
 #Reset the monitor and set powersave
-Popen(['sudo sh -c "echo 1 > /sys/devices/system/cpu/cpufreq/policy0/stats/reset"'], shell=True)
+Popen(['sudo sh -c "echo 1 > /sys/devices/system/cpu/cpufreq/policy0/stats/reset"'], shell=True).wait() 
 
 for sample in range(num_sample):
 
-    t2 = time.time()
+    t_start = time.time()
+    
     stream.start_stream()
     buffer.seek(0)  # pointer rewind
-
-    # it does 46 cycles
-    # each cycle takes 21.74 milliseconds
+    
+    # it does 20 cycles
+    # each cycle takes 50 milliseconds
     # the Popen instantiation takes 50 milliseconds
     # th Popen instantiation + scaling_governor take 150 millisecond --> 50 milliseconds are "sunk"
-    # anticipate 100/21.74 = 4 for loops --> scaling_governor at iteration i = 45-9 = 41
+    # anticipate 100/21.74 = 3 for loops --> scaling_governor at iteration i = (20-3) -1 = 16
 
-    for i in range(int(RATE/CHUNK_SIZE*L)):
-        buffer.write(stream.read(CHUNK_SIZE, exception_on_overflow=False))
-        if i == 38:
+    for i in range(n_cycles):
+        
+        data = stream.read(num_frames=CHUNK_SIZE, exception_on_overflow=False)
+        buffer.write(data)
+        if i == 16:
             t = Thread(target=performance)
             t.start()
 
-    t1 = time.time()
+    
+    t_rec = time.time()
     stream.stop_stream()
     buffer.seek(0)
 
@@ -72,7 +83,7 @@ for sample in range(num_sample):
     spectrogram = tf.abs(stft)
 
     mel_spectrogram = tf.tensordot(spectrogram, linear_to_mel_weight_matrix, 1)
-    mel_spectrogram.set_shape((48, 40))
+    mel_spectrogram.set_shape(spectrogram.shape[:-1].concatenate(linear_to_mel_weight_matrix.shape[-1:]))
     log_mel_spectrogram = tf.math.log(mel_spectrogram + 1e-6)
 
     # compute mfccs
@@ -84,12 +95,13 @@ for sample in range(num_sample):
     tf.io.write_file(new_file_name, mfcc_serialized)
     t = Thread(target=powersave)
     t.start()
-    print(f"Complete time: {time.time() - t2}")
-    print(f'Preprocessing only: {time.time() - t1}')
+    t_stop = time.time()
+    print(f"Complete time: {t_stop - t_start}")
+    print(f"Recording time: {t_rec - t_start}")
+    
 
-
-
-stream.close()
+    
+stream.close()    
 p.terminate()
 #Print the total time for the different VF levels
 Popen(['cat /sys/devices/system/cpu/cpufreq/policy0/stats/time_in_state'], shell=True)
